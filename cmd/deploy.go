52ccd791127afb17c1e6912685decc726e0b7a6c
Moritz Clasmeier <111092021+mclasmeier@users.noreply.github.com>
2026-01-19 10:08:02 +0100

    Disable sub-shell spawning if running without a controlling terminal (#33)
    Co-authored-by: Moritz Clasmeier <mclasmeier@redhat.com>
    Co-authored-by: Marcin Owsiany <porridge@redhat.com>

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -53,6 +53,12 @@ func runDeploy(cmd *cobra.Command, args []string) error {
 		log.Dim("Running containerized.")
 	}
 
+	if env.RunningInteractively {
+		log.Dim("Running with a controlling terminal.")
+	} else {
+		log.Dim("Running without a controlling terminal.")
+	}
+
 	component := "both"
 	if len(args) > 0 {
 		component = args[0]
@@ -60,6 +66,10 @@ func runDeploy(cmd *cobra.Command, args []string) error {
 
 	if (component == "central" || component == "both") && os.Getenv("ROXIE_SHELL") != "" {
 		return errors.New("already in a roxie sub-shell (ROXIE_SHELL environment variable is set), please exit the shell and try again")
+	}
+
+	if !env.RunningInteractively && envrc == "" {
+		return errors.New("running without a controlling terminal requires --envrc to be set")
 	}
 
 	if envrc != "" && portForwarding {
@@ -116,6 +126,7 @@ func runDeploy(cmd *cobra.Command, args []string) error {
 	}
 
 	if envrc != "" {
+		log.Dimf("Writing environment variables to %s", envrc)
 		d.SetEnvrcFile(envrc)
 	}
 
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/fatih/color v1.16.0
 	github.com/moby/sys/mountinfo v0.7.2
 	github.com/spf13/cobra v1.8.0
+	golang.org/x/term v0.38.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -14,5 +15,5 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
 )
