0264601d589bcbccc86f1bca0b862c242cf6b2ea
Moritz Clasmeier <mclasmeier@redhat.com>
2026-06-01 12:31:35 +0200

    CentralDeploymentInfo: Include username

--- internal/types/central_deployment_info.go
+++ internal/types/central_deployment_info.go
@@ -3,6 +3,7 @@ package types
 // CentralDeploymentInfo holds the state of a Central deployment.
 type CentralDeploymentInfo struct {
 	Endpoint       string
+	Username       string
 	Password       string
 	KubeContext    string
 	Exposure       Exposure
