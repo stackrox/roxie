458c4343dd5af5d7dbbfb17c06ce7b54aeb39aaf
Moritz Clasmeier <mclasmeier@redhat.com>
2026-06-09 10:03:53 +0200

    New function for retrieving list of main-tagged images for ACS

---
+++ internal/deployer/acs_images.go
@@ -0,0 +1,34 @@
+package deployer
+
+import "fmt"
+
+const (
+	imageRegistry = "quay.io/rhacs-eng"
+)
+
+func imagesForConfig(config Config) []string {
+	images := make([]string, 0)
+	prefix := ""
+	if config.Roxie.KonfluxImages {
+		prefix = "release-"
+	}
+
+	images = append(images, fmt.Sprintf("%s/%s%s:%s", imageRegistry, prefix, "main", config.Roxie.Version))
+	images = append(images, fmt.Sprintf("%s/%s%s:%s", imageRegistry, prefix, "central-db", config.Roxie.Version))
+	images = append(images, fmt.Sprintf("%s/%s%s:%s", imageRegistry, prefix, "scanner-v4-db", config.Roxie.Version))
+	images = append(images, fmt.Sprintf("%s/%s%s:%s", imageRegistry, prefix, "scanner-v4", config.Roxie.Version))
+	if !config.Roxie.KonfluxImages {
+		prefix = "stackrox-"
+	}
+	images = append(images, fmt.Sprintf("%s/%s%s:%s", imageRegistry, prefix, "operator", config.Operator.Version))
+	images = append(images, fmt.Sprintf("%s/%s%s:v%s", imageRegistry, prefix, "operator-bundle", config.Operator.Version))
+
+	return images
+}
+
+func OperatorBundleImage(config Config) string {
+	if config.Roxie.KonfluxImages {
+		return fmt.Sprintf("%s/release-operator-bundle:v%s", imageRegistry, config.Operator.Version)
+	}
+	return fmt.Sprintf("%s/stackrox-operator-bundle:v%s", imageRegistry, config.Operator.Version)
+}
