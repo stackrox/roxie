cf9d374f67398d3fb3fd47be74036594c09957ca
Moritz Clasmeier <mclasmeier@redhat.com>
2026-06-29 17:40:22 +0200

    Add test for VerifyImageExistence NotFound behavior

--- internal/helpers/tag_integration_test.go
+++ internal/helpers/tag_integration_test.go
@@ -4,15 +4,20 @@ package helpers
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net/http"
 	"testing"
 	"time"
 
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
 	"github.com/stackrox/roxie/internal/constants"
 	"github.com/stackrox/roxie/internal/logger"
+	"github.com/stackrox/roxie/internal/ocihelper"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -33,4 +38,18 @@ func TestLookupLatestTag_Integration(t *testing.T) {
 	require.NoError(t, err, "image %s is not pullable", imageRef)
 
 	t.Logf("Latest pullable tag: %s (%s)", tag, imageRef)
+}
+
+func TestVerifyImageExistence_NotFound_Integration(t *testing.T) {
+	log := logger.New()
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	madeUpImage := fmt.Sprintf("%s/main:99.99.99", constants.DefaultRegistry)
+	err := ocihelper.VerifyImageExistence(ctx, log, madeUpImage)
+	require.Error(t, err)
+
+	var te *transport.Error
+	require.True(t, errors.As(err, &te), "expected transport.Error, got %T", err)
+	assert.Equal(t, http.StatusNotFound, te.StatusCode)
 }
