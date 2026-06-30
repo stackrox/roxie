aad4bedbae98da8991a210bf44047a10f9993450
Moritz Clasmeier <mclasmeier@redhat.com>
2026-06-30 09:46:52 +0200

    Add REGISTRY_* env var names to constants package

--- internal/constants/registries.go
+++ internal/constants/registries.go
@@ -3,4 +3,7 @@ package constants
 const (
 	DefaultRegistry    = "quay.io/rhacs-eng"
 	GitHubStackroxRepo = "stackrox/stackrox"
+
+	EnvRegistryUsername = "REGISTRY_USERNAME"
+	EnvRegistryPassword = "REGISTRY_PASSWORD"
 )
