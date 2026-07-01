5702216e683d6fc99d99c01039115df9336786d0
Moritz Clasmeier <mclasmeier@redhat.com>
2026-07-01 09:26:18 +0200

    Fix integration test

--- internal/helpers/tag_integration_test.go
+++ internal/helpers/tag_integration_test.go
@@ -10,7 +10,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
@@ -34,7 +33,7 @@ func TestLookupLatestTag_Integration(t *testing.T) {
 	ref, err := name.ParseReference(imageRef)
 	require.NoError(t, err)
 
-	_, err = remote.Head(ref, remote.WithContext(ctx), remote.WithAuthFromKeychain(authn.DefaultKeychain))
+	_, err = remote.Head(ref, remote.WithContext(ctx), remote.WithAuthFromKeychain(ocihelper.Keychain))
 	require.NoError(t, err, "image %s is not pullable", imageRef)
 
 	t.Logf("Latest pullable tag: %s (%s)", tag, imageRef)
