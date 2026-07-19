06addee75d180683ca8c85a631218db388d3e533
Moritz Clasmeier <mclasmeier@redhat.com>
2026-07-19 21:17:49 +0200

    Add function InsecureRandomString

---
+++ internal/helpers/random.go
@@ -0,0 +1,15 @@
+package helpers
+
+import "math/rand/v2"
+
+const (
+	charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
+
+func InsecureRandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = charset[rand.IntN(len(charset))]
+	}
+	return string(b)
+}
